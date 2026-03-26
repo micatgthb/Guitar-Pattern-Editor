6ac350518d7947f04a30a55330c3a1315e39f495
micatgthb <somemacmeikel@mac.com>
2026-03-26 03:29:59 +0100

    Update fretboard.js

--- js/fretboard.js
+++ js/fretboard.js
@@ -833,17 +833,17 @@ document.addEventListener("DOMContentLoaded",()=>{
   const startFretInput = document.getElementById("startFret")
   const endFretInput = document.getElementById("endFret")
 
-    [startFretInput, endFretInput].forEach(input=>{
-      input?.addEventListener("change", ()=>{
-        if(isAuto()){
-          build()
-          drawStrings()
-          applyScale()
-          drawSequenceLines()
-          drawFretMarkers()
-        }
-      })
+  [startFretInput, endFretInput].forEach(input=>{
+    input?.addEventListener("change", ()=>{
+      if(isAuto()){
+        build()
+        drawStrings()
+        applyScale()
+        drawSequenceLines()
+        drawFretMarkers()
+      }
     })
+  })
 
 if(saveBtn){
   saveBtn.addEventListener("click", savePattern)
@@ -936,7 +936,8 @@ updateSequenceButton()
   
   
 
-inst.addEventListener("change",()=>{
+if(inst){
+  inst.addEventListener("change",()=>{
   stopPlayback()
 
   if(isAuto()){
